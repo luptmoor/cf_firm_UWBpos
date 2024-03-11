31fe28acf8f463fe0e08674e4eb3adb0b8e994c7
luptmoor <lukas.uptmoor@web.de>
2024-03-11 11:18:05 +0100

    changed NumUWB to 4, added flashing script. Flashable!

---
+++ flash.sh
@@ -0,0 +1,8 @@
+echo "Flashing Drone 25 (E5)"
+cfloader flash cf2.bin stm32-fw -w radio://0/100/2M/E7E7E7E7E5
+echo "Flashing Drone 26 (E6)"
+cfloader flash cf2.bin stm32-fw -w radio://0/100/2M/E7E7E7E7E6
+echo "Flashing Drone 27 (E7)"
+cfloader flash cf2.bin stm32-fw -w radio://0/100/2M/E7E7E7E7E7
+echo "Flashing Drone 30 (E8)"
+cfloader flash cf2.bin stm32-fw -w radio://0/100/2M/E7E7E7E7E8
